754a258732994b9acb02b161a19e8630cc61fc23
gahme <g_ahmeddd@yahoo.com>
2024-11-09 20:33:35 +0300

    Added 74

---
+++ solutions/medium/74.py
@@ -0,0 +1,29 @@
+#Problem 74: Search a 2D Matrix
+#Difficulty: Medium
+#URL: https://leetcode.com/problems/search-a-2d-matrix
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+
+        m, n = len(matrix), len(matrix[0])
+        l, r = 0, m*n - 1
+
+        while l <= r:
+            mid = l + (r - l) // 2
+            row = mid // n
+            col = mid % n
+
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+        return False
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3))  # True
+    print(s.searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 13))  # False
