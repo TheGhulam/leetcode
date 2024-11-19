b68ac315e5ad490ea2e8581b6b496bebbb591ba1
gahme <g_ahmeddd@yahoo.com>
2024-11-19 10:49:19 +0300

    testing

--- solutions/medium/2461.py
+++ solutions/medium/2461.py
@@ -3,30 +3,6 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
         o = 0
-        count = defaultdict(int)
-        s = 0
-
-        l = 0
-        for r in range(0, len(nums)):
-            s += nums[r]
-            count[nums[r]] += 1
-
-            if r-l+1 > k:
-                s -= nums[l]
-                count[nums[l]] -= 1
-                if count[nums[l]] == 0: count.pop(nums[l])
-                l += 1
-
-            if len(count) == r - l + 1 == k:
-                o = max(o, s)
-        
-        return o
-
-    
-# Optimization version
-class Solution:
-    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        o = 0
         last_i = {}
         s = 0
 
