beb5d214c0ee1d951ae9a93f3fa9964cc20c75df
gahme <g_ahmeddd@yahoo.com>
2024-11-19 10:47:58 +0300

    optimized

--- solutions/medium/2461.py
+++ solutions/medium/2461.py
@@ -21,3 +21,26 @@ class Solution:
                 o = max(o, s)
         
         return o
+
+    
+# Optimization version
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        o = 0
+        last_i = {}
+        s = 0
+
+        l = 0
+        for r in range(0, len(nums)):
+            s += nums[r]
+            i = last_i.get(nums[r], -1)
+            last_i[nums[r]] = r
+            
+            while l <= i or r-l+1 > k:
+                s -= nums[l]
+                l += 1
+
+            if r - l + 1 == k:
+                o = max(o, s)
+        
+        return o
